6ad80598aebc808a689fc50bec1bd1c548d665ec
Piotr Kołodyński <kolek>
2018-11-30 17:58:21 +0100

    Dodano galerie

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,15 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def getUser(name):
-    return render_template('user.html', name=name)
+@app.route('/galeria')
+def getGaleria():
+    obrazki = [
+        'https://bit.ly/2Paw95n',
+        'https://bit.ly/2Paw95n',
+        'https://bit.ly/2Paw95n',
+        'https://bit.ly/2Paw95n',
+    ]
+    return render_template('galeria.html', obrazki=obrazki)
+
+
 
